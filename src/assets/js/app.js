30de745647f5f718c2831c00ad4e143075f3fee7
Alvin Raminger <alvin.raminger@snk.de>
2020-05-18 15:12:39 +0200

    Added functionality for mobile sidebar

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -7,5 +7,9 @@ $(document).ready(function(){
     $('.js-accordion dt, .js-toggle').click(function(){
         $($(this).data('target') ? $(this).data('target') : $(this)).toggleClass('active');
     });
+    $('.js-menu .js-scroll-navigate a').click(function(){
+        $(this).parents('.js-menu').first().removeClass('active');
+        console.log("click");
+    })
 })
 
