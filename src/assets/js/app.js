6012877d4bad08c4499efb5cae657c634934b57b
Alvin Raminger <alvin.raminger@snk.de>
2020-05-19 20:30:52 +0200

    minor adjustment on faq mobile state (initial)

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,10 +4,10 @@ import $ from 'jquery';
 window.jQuery = $;
 
 $(document).ready(function(){
-    $('.js-accordion dt, .js-toggle').click(function(){
+    $('.js-accordion dt, .js-toggle').on('click tap', function(){
         $($(this).data('target') ? $(this).data('target') : $(this)).toggleClass('active');
     });
-    $('.js-menu .js-scroll-navigate a').click(function(){
+    $('.js-menu .js-scroll-navigate a').on('click tap', function(){
         $(this).parents('.js-menu').first().removeClass('active');
         $(this).parents('.js-menu').first().find('a').removeClass('active');
         $(this).addClass('active');
