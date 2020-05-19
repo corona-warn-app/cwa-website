4ad9f0a84f78fa69a0899790d914ada64efe8852
Malte Janduda <mail@janduda.net>
2020-05-19 17:57:39 +0200

    first release

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,8 +1,16 @@
 import $ from 'jquery';
-import 'what-input';
+//import 'what-input';
 
 window.jQuery = $;
 
-console.log('loaded');
-console.log(typeof($) == 'function' ? 'jQuery loaded' : 'loading issue');
+$(document).ready(function(){
+    $('.js-accordion dt, .js-toggle').click(function(){
+        $($(this).data('target') ? $(this).data('target') : $(this)).toggleClass('active');
+    });
+    $('.js-menu .js-scroll-navigate a').click(function(){
+        $(this).parents('.js-menu').first().removeClass('active');
+        $(this).parents('.js-menu').first().find('a').removeClass('active');
+        $(this).addClass('active');
+    })
+})
 
