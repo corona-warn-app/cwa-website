c2f36b30c6ca4c8b77730ae37e69172e0c90688b
hangy <hangy@hangy.de>
2020-06-16 14:15:51 +0200

    Use navigator.languages for redirect, if available.

--- src/assets/js/static/redirect.js
+++ src/assets/js/static/redirect.js
@@ -1,6 +1,23 @@
-if((navigator.language || navigator.userLanguage).toLowerCase().indexOf('de') >= 0) {
-  window.location.href = "./de/";
+var href = "./en/";
+if (
+  typeof navigator.languages !== "undefined" &&
+  navigator.languages.length > 0
+) {
+  for (var i = 0; i < navigator.languages.length; ++i) {
+    var lowerCaseValue = navigator.languages[i].toLowerCase();
+    if (lowerCaseValue.indexOf("de") >= 0) {
+      href = "./de/";
+      break;
+    } else if (lowerCaseValue.indexOf("en") >= 0) {
+      href = "./en/";
+      break;
+    }
+  }
+} else if (
+  (navigator.language || navigator.userLanguage).toLowerCase().indexOf("de") >=
+  0
+) {
+  href = "./de/";
 }
-else {
-  window.location.href = "./en/";
-}
+
+window.location.href = href;
