53e61d7a1f0ca7843022ffbfbd9b07fddcb6fdf9
Sebastian Wolf <sebastian.wolf@sap.com>
2020-07-15 13:38:24 +0200

    fix: Consistently use DIN 5008 phone numbers

--- cypress/integration/hotline_spec.js
+++ cypress/integration/hotline_spec.js
@@ -2,11 +2,11 @@ describe('Test Hotline phone number', () => {
   
   it('Should contain App Hotline', () => {
     cy.visit('http://localhost:8000/')
-    cy.contains('+498007540001')
+    cy.contains('+49 800 7540001')
   })
 
   it('Should contain TAN Hotline', () => {
     cy.visit('http://localhost:8000/')
-    cy.contains('+498007540002')
+    cy.contains('+49 800 7540002')
   })
 })
