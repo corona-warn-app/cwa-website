73d052c548603857c8be5ceff866b653863e3207
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2020-08-06 16:12:48 +0200

    slider optimization, cleanup, text updates

--- cypress/prepare.sh
+++ cypress/prepare.sh
@@ -5,6 +5,3 @@ BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cp $BASE_DIR/fixtures/*.md $BASE_DIR/../blog/
 cp $BASE_DIR/fixtures/img/*.* $BASE_DIR/../blog/img/
-mkdir -p $BASE_DIR/../cypress/integration/__image_snapshots__
-
-npm run build
