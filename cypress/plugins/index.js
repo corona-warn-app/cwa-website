f07547002e1486dc33ed7bd5a29da10f4d270082
Markus Edenhauser <1720843+maxmarkus@users.noreply.github.com>
2020-08-07 12:50:01 +0200

    update visual comparison image, set viewport

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -3,22 +3,22 @@ const { initPlugin } = require('cypress-plugin-snapshots/plugin');
 module.exports = (on, config) => {
   initPlugin(on, config);
 
-  const viewport = { w: 1280, h: 720 }
+  const viewport = { viewportWidth: 1280, viewportHeight: 720 }
   // prevent different screen sizes according to
   // https://github.com/meinaart/cypress-plugin-snapshots/issues/104
   on('before:browser:launch', (browser = {}, launchOptions) => {
     switch (browser.name) {
       //browser.family === 'chromium' && browser.name !== 'electron')
       case 'chrome':
-        launchOptions.args.push(`--window-size=$ viewport.w},$ viewport.h}`)
+        launchOptions.args.push(`--window-size=$ viewport.viewportWidth},$ viewport.viewportHeight}`)
         /*
         launchOptions.push('--cast-initial-screen-width=1600')
         launchOptions.push('--cast-initial-screen-height=900')
           */
         break
       case 'electron':
-        launchOptions.preferences.width = viewport.w
-        launchOptions.preferences.height = viewport.h
+        launchOptions.preferences.width = viewport.viewportWidth
+        launchOptions.preferences.height = viewport.viewportHeight
         break
     }
     return launchOptions
