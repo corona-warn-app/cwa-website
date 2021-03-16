722a719357dd8816e2da8385b700555d2fd18d06
Sascha-Matthias Kulawik <sascha.kulawik@cuisine.digital>
2021-03-16 15:10:32 +0100

    Using bootstrap cols

--- src/services/blog-processor.js
+++ src/services/blog-processor.js
@@ -62,7 +62,7 @@ const replaceImagePaths = (content, folderName) => {
 }
 
 const replaceVideo = (content, folderName) => {
-  videoHtml = '<div id="player-overlay"><video controls preload="metadata" poster="$2.jpg"><source src="$2.mp4" type="video/mp4"></source><source src="$2.webm" type="video/webm"></source></video></div>'
+  videoHtml = '<div class="col-6 col-md-6" id="player-overlay"><video controls preload="metadata" poster="$2.jpg"><source src="$2.mp4" type="video/mp4"></source><source src="$2.webm" type="video/webm"></source></video></div>'
   const c = content.replace(/({{)([^>]*)(}})/gi, videoHtml);
   return c
 }
