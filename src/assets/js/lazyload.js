214144f5c9948b89d13f8409ced7f2a20120925a
Sascha-Matthias Kulawik <sascha.kulawik@cuisine.digital>
2021-03-22 14:09:04 +0100

    Added IE11 support

--- src/assets/js/lazyload.js
+++ src/assets/js/lazyload.js
@@ -1,23 +1,43 @@
 function lazyload() {
-  const imageObserver = new IntersectionObserver((entries, imgObserver) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        const lazyImage = entry.target;
-        if(lazyImage.hasAttribute("data-src")) {
-          lazyImage.src = lazyImage.dataset.src;    
-        } else {
-          lazyImage.srcset = lazyImage.dataset.srcset;
+  const images = Array.prototype.slice.call(document.querySelectorAll(".lazy"));
+
+  if ('IntersectionObserver' in window &&
+    'IntersectionObserverEntry' in window &&
+    'intersectionRatio' in window.IntersectionObserverEntry.prototype) {
+    var imageObserver = new IntersectionObserver(function (entries, observer) {
+      for (let i=0; i<entries.length; i++) {
+        let entry = entries[i];
+//      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          const lazyImage = entry.target;
+          if(lazyImage.hasAttribute("data-src")) {
+            lazyImage.src = lazyImage.dataset.src;    
+          } else {
+            lazyImage.srcset = lazyImage.dataset.srcset;
+          }
+          lazyImage.classList.remove("lazy");
+          imageObserver.unobserve(lazyImage);
         }
-        lazyImage.classList.remove("lazy");
-        imgObserver.unobserve(lazyImage);
       }
     });
-  });
 
-  const images = document.querySelectorAll(".lazy");
-  images.forEach((image) => {
-    imageObserver.observe(image);
-  });
+    for (let i=0; i<images.length; i++) {
+      let image = images[i];
+      imageObserver.observe(image);
+    }
+  } else {
+    console.log("No IntersectionObserver - no lazy loading");
+    //images.forEach((image) => {
+    for (let i=0; i<images.length; i++) {
+      let image = images[i];
+      if(image.hasAttribute("data-src")) {
+        image.src = image.dataset.src;    
+      } else {
+        image.srcset = image.dataset.srcset;
+      }
+      image.classList.remove("lazy");
+    }
+  }
 }
 
 window.onload = lazyload;
