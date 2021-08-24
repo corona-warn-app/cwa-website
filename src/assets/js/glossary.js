ed6df3ddf2cf61edfbd7a66e94d5d3b5cdcc306c
Sven Gabriel <svengabr@googlemail.com>
2021-08-24 10:02:42 +0200

    imported jquery

--- src/assets/js/glossary.js
+++ src/assets/js/glossary.js
@@ -1,2 +1,31 @@
 
-console.log(window.location.hash[10]);
+import $ from 'jquery';
+
+window.jQuery = $;
+
+let hash = window.location.hash;
+
+function checkGlossaryAnchor(anchor) {
+    if(hash.indexOf("glossary_") > -1) {
+        // anchor starts with glossary_
+        console.log(hash[10]);
+    } else {
+        // anchor doesnt start with glossary_
+    }
+}
+
+
+
+
+
+
+
+
+$('a').on("click", function(e) {
+    let anchor = "";
+    // todo: extract anchor from link
+
+    checkGlossaryAnchor(anchor);
+});
+
+checkGlossaryAnchor(hash);
