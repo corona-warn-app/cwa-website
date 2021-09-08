459be671d5474490d5860559e05b0acf283b45cf
dsarkar <dipankar.sarkar@cuisine.digital>
2021-09-08 10:20:36 +0200

    Release 2.9 updates (#1692)
    * Update Privacy notice 2.9 - (there was no privacy notice update 2.8) (#1687)
    * Update gulpfile
    * Add screenshots for release 2.9 (#1707)
    * Update FAQ entries about recovery vaccination status (#1714)
    * Add FAQ entries: vac_cert_booster_info, vac_cert_booster_cwa and warn_for_others (#1712)

--- gulpfile.js
+++ gulpfile.js
@@ -354,10 +354,10 @@ function replaceVersionNumbers() {
     .src([PATHS.dist + "/**/*.html"])
     .pipe(replace('[ios.latest-os-version]', '14.7.1'))
     .pipe(replace('[ios.minimum-required-os-version]', '12.5'))
-    .pipe(replace('[ios.current-app-version]', '2.8.1'))
+    .pipe(replace('[ios.current-app-version]', '2.9.0'))
     .pipe(replace('[android.latest-os-version]', '11'))
     .pipe(replace('[android.minimum-required-os-version]', '6'))
-    .pipe(replace('[android.current-app-version]', '2.8.0'))
+    .pipe(replace('[android.current-app-version]', '2.9.0'))
     .pipe(replace('[last-update]', new Date().toISOString().split('T')[0]))
     .pipe(gulp.dest(PATHS.dist))
 }
