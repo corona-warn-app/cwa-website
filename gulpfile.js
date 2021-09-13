238039fd274cf833f48e2b309729cb30a420b796
Tim <67682506+Ein-Tim@users.noreply.github.com>
2021-09-13 19:45:55 +0200

    Bump current iOS version
    This commit bumps the current iOS version from iOS 14.7.1 to 14.8.

--- gulpfile.js
+++ gulpfile.js
@@ -352,7 +352,7 @@ function createFaqRedirects() {
 function replaceVersionNumbers() {
   return gulp
     .src([PATHS.dist + "/**/*.html"])
-    .pipe(replace('[ios.latest-os-version]', '14.7.1'))
+    .pipe(replace('[ios.latest-os-version]', '14.8'))
     .pipe(replace('[ios.minimum-required-os-version]', '12.5'))
     .pipe(replace('[ios.current-app-version]', '2.9.0'))
     .pipe(replace('[android.latest-os-version]', '11'))
