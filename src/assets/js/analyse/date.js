d7113bd8308f262828c6b4a94bca6ee2baed130d
Fabio Fuschi <fabio@snk-interactive.de>
2021-09-14 12:03:08 +0200

    analysis: - change date Picker behavior

--- src/assets/js/analyse/date.js
+++ src/assets/js/analyse/date.js
@@ -67,8 +67,8 @@ $(() => {
 		format: (documentLang == "de")? "DD.MM.YYYY": "MM/DD/YYYY",
 		allowRepick: true,
 		buttonText: {
-			"previousMonth":'<svg width="23" height="24" viewBox="0 0 23 24" xmlns="http://www.w3.org/2000/svg"><path d="M13.8912 21L5.75 12.75L13.8912 4.5L15.8125 6.447L9.56007 12.75L15.8125 19.053L13.8912 21Z" fill="#006082"/></svg>',
-			"nextMonth":'<svg width="23" height="24" viewBox="0 0 23 24" xmlns="http://www.w3.org/2000/svg"><path d="M9.10882 3L17.25 11.25L9.10882 19.5L7.1875 17.553L13.4399 11.25L7.1875 4.947L9.10882 3Z" fill="#006082"/></svg>'
+			"previousMonth": '<svg width="23" height="24" viewBox="0 0 23 24" xmlns="http://www.w3.org/2000/svg"><path d="M13.8912 21L5.75 12.75L13.8912 4.5L15.8125 6.447L9.56007 12.75L15.8125 19.053L13.8912 21Z" fill="#006082"/></svg>',
+			"nextMonth": '<svg width="23" height="24" viewBox="0 0 23 24" xmlns="http://www.w3.org/2000/svg"><path d="M9.10882 3L17.25 11.25L9.10882 19.5L7.1875 17.553L13.4399 11.25L7.1875 4.947L9.10882 3Z" fill="#006082"/></svg>'
 		},
 		setup: (picker) => {
 			picker.on('clear:selection', () => {
@@ -77,9 +77,27 @@ $(() => {
 				picker.setDateRange(...dateStr);
 			   $(".analyseRangePicker").removeClass("active")
 			}); 
+
+			picker.on('before:click', (target) => {
+				if(!$(target).hasClass("day-item") || $(target).hasClass("is-locked") || $(target).hasClass("is-end-date")) return;
+				picker.preventClick = true;
+
+				const cDate = DateTime.fromMillis($(target).data("time"));
+				let date = picker.getDateRange();
+				const diff = date.map(e => Math.abs(cDate.diff(e).as('days')));
+				date[diff.indexOf(Math.min(...diff))] = cDate;
+				picker.setDateRange(...date);
+			});
 		}
 	});
 
+
+	picker.getDateRange = () => {
+		return [
+			DateTime.fromJSDate(picker.getStartDate().dateInstance),
+			DateTime.fromJSDate(picker.getEndDate().dateInstance)
+		];
+	};
 
 	$('.analyseRangePicker-input').each(function(){
 		new Cleave(this, {
@@ -120,10 +138,7 @@ $(() => {
 	$(document).on("click",".analyseRangePicker-apply", function(){
 		if(!picker.getStartDate() || !picker.getEndDate()) return;
 		$(".analyseRangeRadio input:checked").prop('checked', false);
-		date$.next([
-			DateTime.fromJSDate(picker.getStartDate().dateInstance).toISODate(), 
-			DateTime.fromJSDate(picker.getEndDate().dateInstance).toISODate()
-		]);
+		date$.next(picker.getDateRange().map(e => e.toISODate()));
 	});
 
 
