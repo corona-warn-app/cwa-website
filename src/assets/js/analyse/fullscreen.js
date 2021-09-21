06af166be45a685e3db5c344615e9970824cb35d
Fabio Fuschi <fabio@snk-interactive.de>
2021-09-21 16:45:17 +0200

    analysis: - add chart box expand function

--- src/assets/js/analyse/fullscreen.js
+++ src/assets/js/analyse/fullscreen.js
@@ -1,12 +1,16 @@
 import $ from 'jquery';
+import ApexCharts from 'apexcharts'
 import screenfull from 'screenfull'
 
 $(document).on("click",".analyseBoard-fullscreen", function(e){
 	e.preventDefault()
 	const $e = $(this).parents(".analyseBoard");
+	const height = ($("body").hasClass("fullscreen"))? 230: (window.innerHeight - 100); 
 	$e.add("body").toggleClass("fullscreen");
 
-	if (screenfull.isEnabled) screenfull.toggle($e.get(0));
+	ApexCharts.exec($(this).data("id"), "updateOptions", { chart: { height } }, true, false, false);
+
+	if (screenfull.isEnabled) screenfull.toggle($e.first());
 });
 
 
