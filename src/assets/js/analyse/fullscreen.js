f76d9c020c781ab3bcc27b4f9f758c5ddcdea643
Fabio Fuschi <fabio@snk-interactive.de>
2021-09-28 14:19:27 +0200

    analysis: - fix mobile fullscreen

--- src/assets/js/analyse/fullscreen.js
+++ src/assets/js/analyse/fullscreen.js
@@ -5,7 +5,7 @@ import screenfull from 'screenfull'
 $(document).on("click",".analyseBoard-fullscreen", function(e){
 	e.preventDefault()
 	const $e = $(this).parents(".analyseBoard");
-	const height = ($("body").hasClass("fullscreen"))? 230: (window.innerHeight - 100); 
+	const height = ($("body").hasClass("fullscreen"))? 230: (window.innerHeight - 220); 
 	$e.add("body").toggleClass("fullscreen");
 
 	ApexCharts.exec($(this).data("id"), "updateOptions", { chart: { height } }, true, false, false);
