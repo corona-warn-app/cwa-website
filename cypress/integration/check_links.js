ffb3c8d84ecab536aa0ec08fcd08247ee431fdeb
dsarkar <dipankar.sarkar@cuisine.digital>
2021-10-01 15:33:59 +0200

    fix filter (email, tel)

--- cypress/integration/check_links.js
+++ cypress/integration/check_links.js
@@ -1,12 +1,37 @@
 const { softAssert, softExpect } = chai;
 
 context("Check for broken links", () => {
-  const pages = [ '/en/community', '/de/community', '/en/faq', '/de/faq','/en/rat-partner/','/en/terms-of-use/','/en/privacy/']
+  const pages = [ '/de',
+                  '/en',
+                  '/de/eventregistration/',
+                  '/en/eventregistration/',
+                  '/de/community/',
+                  '/en/community/',
+                  '/de/science/',
+                  '/en/science/',
+                  '/de/analysis/',
+                  '/en/analysis/',
+                  '/de/blog/',
+                  '/en/blog/',
+                  '/de/screenshots/',
+                  '/en/screenshots/',
+                  '/de/faq/',
+                  '/en/faq/',
+                  '/de/rat-partner/',
+                  '/en/rat-partner/',
+                  '/de/privacy/',
+                  '/en/privacy/',
+                  '/de/terms-of-use/',
+                  '/en/terms-of-use/',
+                  '/de/event-qr-code-guide/',
+                  '/en/event-qr-code-guide/'
+                ]
+
   pages.forEach(page => {
     it(`"${page}" - Check for broken links`, () => {
       cy.visit({log: false, url: page} )
       // filter / reg-ex mailto, tel does not work
-      cy.get("a[href]:not([href^='mailto:'], [href$='tel:'])").each(url => {
+      cy.get("a:not([href*='mailto:'],[href*='tel:'])").each(url => {
       // ToDo: remove duplicates e.g. #top
         if (url.prop('href') ) { 
           cy.request({
