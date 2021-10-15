0fc2aaae621fb3b0a354765b321e3cf1e10c47a8
Brian Fernández Muñiz <brifemu@gmail.com>
2021-10-15 10:25:23 +0200

    Divide science scripts and remove science.js

---
+++ src/assets/js/science/table.js
@@ -0,0 +1,12 @@
+
+
+// add bootstrap table styles to pandoc tables
+function bootstrapStylePandocTables() {
+    $('tr.odd').parent('tbody').parent('table').addClass('table table-condensed');
+  }
+  $(document).ready(function () {
+    bootstrapStylePandocTables();
+  });
+  
+  
+  
