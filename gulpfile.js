3be5bb159102c8d073d780feae56e1411b757eff
Ein-Tim <67682506+Ein-Tim@users.noreply.github.com>
2021-11-09 21:49:01 +0100

    Bump Android app version
    This commit bumps the current Android app version from 2.13.2 to 2.13.3.

--- gulpfile.js
+++ gulpfile.js
@@ -428,7 +428,7 @@ function replaceVersionNumbers() {
     .pipe(replace('[ios.current-app-version]', '2.13.1'))
     .pipe(replace('[android.latest-os-version]', '12'))
     .pipe(replace('[android.minimum-required-os-version]', '6'))
-    .pipe(replace('[android.current-app-version]', '2.13.2'))
+    .pipe(replace('[android.current-app-version]', '2.13.3'))
     .pipe(replace('[last-update]', new Date().toISOString().split('T')[0]))
     .pipe(gulp.dest(PATHS.dist))
 }
