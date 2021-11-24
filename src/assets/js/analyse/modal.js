140bfda1111ac04bd9d3f44c6c873bda28923ecc
Brian Fernández Muñiz <brifemu@gmail.com>
2021-11-24 13:33:03 +0100

    Modal message clicking on info icon

--- src/assets/js/analyse/modal.js
+++ src/assets/js/analyse/modal.js
@@ -10,4 +10,14 @@ $(document).on("click",".analyseModal-close", function(e){
 	$(".analyseModal, .analyseModalBg").removeClass("active")
 });
 
+$(document).on("click",".analyseBoard-info.info-textblock", function(e){
+	e.preventDefault();
+	$(`.analyseModalInfo[rel="${$(this).attr("rel")}"], .analyseModalBg`).addClass("active")
+});
+
+$(document).on("click",".analyseModalInfo-close", function(e){
+	e.preventDefault()
+	$(".analyseModalInfo, .analyseModalBg").removeClass("active")
+});
+
 export default null;
