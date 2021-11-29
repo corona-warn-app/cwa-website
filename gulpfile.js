f17cae9a1dcbbab8ae36f66082409d4913e99568
Brian Fernández Muñiz <brifemu@gmail.com>
2021-11-29 08:31:13 +0100

    Fix file name

--- gulpfile.js
+++ gulpfile.js
@@ -140,10 +140,10 @@ function analyseData(){
   }
 
   return fallbackdataFn().then(e => {
-    fs.writeFileSync('src/data/analyse_data_no_internet.json', e, {flag: 'w'});
+    fs.writeFileSync('src/data/analyse-backup.json', e, {flag: 'w'});
     return fs.writeFileSync(`./public/${analyseConfig.fallbackFile}`, e);
   }).catch(e => {
-    const data = fs.readFileSync('src/data/analyse_data_no_internet.json', 'utf8');
+    const data = fs.readFileSync('src/data/analyse-backup.json', 'utf8');
     if(!fs.existsSync("public")) fs.mkdirSync("public")
     return fs.writeFileSync(`./public/${analyseConfig.fallbackFile}`, data, {flag: 'w'});
   })
