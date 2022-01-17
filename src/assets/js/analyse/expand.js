588f08caa2f8b5537a911ed4118218b6bcd033ab
Brian Fernández Muñiz <brifemu@gmail.com>
2022-01-17 11:30:51 +0100

    Revert fix expand functionality

--- src/assets/js/analyse/expand.js
+++ src/assets/js/analyse/expand.js
@@ -7,7 +7,7 @@ $(document).on("click",".analyseBoard-expand", function(){
 	const $e = $(this).parents(".analyseBoard");
 	const $p = $(".analyseBoards")
 
-	const height = ($e.hasClass("expand"))? 230: (window.innerHeight - 220); 
+	const height = ($e.hasClass("expand"))? 230 : 690; 
 	$(".analyseBoards").add($e).toggleClass("expand");
 	ApexCharts.exec($(this).data("id"), "updateOptions", { chart: { height } }, true, false, false);
 });
