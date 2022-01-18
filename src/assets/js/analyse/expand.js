bab2f9ba7e363b1911446672e33289f55f9eb2dd
Brian Fernández Muñiz <brifemu@gmail.com>
2022-01-18 14:07:45 +0100

    Fix responsive graph

--- src/assets/js/analyse/expand.js
+++ src/assets/js/analyse/expand.js
@@ -6,12 +6,30 @@ import ApexCharts from 'apexcharts'
 $(document).on("click",".analyseBoard-expand", function(){
 	const $e = $(this).parents(".analyseBoard");
 	const $p = $(".analyseBoards")
-
-	const height = ($e.hasClass("expand"))? 230: window.innerWidth > window.innerHeight ? (window.innerHeight - 220) : (window.innerWidth - 370);
+	const padding = $($e).css('padding-top').replace("px","").split(" ");
+	let tPadding = 0;
+	padding.forEach(element => {
+		tPadding += parseInt(element);
+	});
+	const spacing = $(".analyseBoard-head").height() + $(".analyseBoard-total").height() + $(".analyseChartLegend").height() + tPadding + 48;
+	const height = ($e.hasClass("expand"))? 230: (window.innerHeight - spacing);
 	$(".analyseBoards").add($e).toggleClass("expand");
 	ApexCharts.exec($(this).data("id"), "updateOptions", { chart: { height } }, true, false, false);
 });
 
+$(window).on("resize", function () {
+	const $e = document.querySelector(".analyseBoard.expand");
+	if($e) {
+		const padding = $($e).css('padding-top').replace("px","").split(" ");
+		let tPadding = 0;
+		padding.forEach(element => {
+			tPadding += parseInt(element);
+		});
+		const spacing = $(".analyseBoard-head").height() + $(".analyseBoard-total").height() + $(".analyseChartLegend").height() + tPadding + 48;
+		const height = window.innerHeight - spacing;
+		ApexCharts.exec($(".analyseBoard-expand").data("id"), "updateOptions", { chart: { height } }, true, false, false);
+	}
+});
 
 
 export default null;
