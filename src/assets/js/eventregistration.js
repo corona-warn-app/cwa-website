27bd5bd13bc9fd0a9afe24af1c3c888dd0d0f453
Brian Fernández Muñiz <brifemu@gmail.com>
2022-04-08 17:39:09 +0200

    Adapt single and multi PDF for readers

--- src/assets/js/eventregistration.js
+++ src/assets/js/eventregistration.js
@@ -16,6 +16,8 @@ import './pdf-font-extrabold'
 const backgroundImage = new Image();
 backgroundImage.src = '/assets/img/pt-poster-1.0.0.png';
 
+const PDF = {single: null, multi: null};
+
 const QR_LIST = [];
 
 function isValidDate(date) {
@@ -88,18 +90,8 @@ document.getElementById('generateQR').addEventListener('click', async function (
     let starttime = document.getElementById('starttime-time').value;
     let enddate = document.getElementById('endtime-date').value.split('.').reverse().join('-');
     let endtime = document.getElementById('endtime-time').value;
-
-    let canvas = document.getElementById('eventqrcode');
-    let ctx = canvas.getContext('2d');
     
     const qr = await GenerateQRCode(grid, description, address, defaultcheckinlengthMinutes, locationtype, startdate, enddate, starttime, endtime, false, defaultcheckinlengthHours);
-    // ctx.width = 1654;
-    // ctx.height = 2339;
-    // canvas.width = 1654;
-    // canvas.height = 2339;
-    // canvas.style.maxWidth = "100%"
-
-    // ctx.drawImage(qr, 0, 0);
 
     PrintLayout(qr)
 
@@ -107,8 +99,7 @@ document.getElementById('generateQR').addEventListener('click', async function (
     // Active download button
     document.getElementById('downloadCode').disabled = false;
     document.getElementById('eventplaceholder').classList.add('d-none');
-      
-    canvas.classList.remove('d-none');   
+    $("#eventqrcode").attr("src", PDF.single.output('datauristring')).removeClass("d-none");
   }
 });
 
@@ -129,15 +120,23 @@ document.getElementById('generateMultiQR').addEventListener('click', function (e
         setTimeout(() => {          
           GenerateMultiQRCode(json).then(function (status) {
             if (status) {             
-              printQRsOnPage().then(function (pages) {
-                printPages(pages)
+              printQRsOnPage().then(function (data) {
+                let info = document.getElementById("qrsGeneratedInfo");
+                let text = info.textContent;
+                text = text.replace('#', data.pages);
+                text = text.replace('$', data.totalQR);
+                info.textContent = text;
+                info.classList.remove('d-none');
+
+                document.getElementById("modal").classList.add('d-none');
+                document.getElementById('generateMultiQR').disabled = false;
+                $("#multieventqrcode").attr("src", PDF.multi.output('datauristring')).removeClass("d-none");
               });
             }              
           });
         },100)
       }  
     });
-    document.getElementById('generateMultiQR').disabled = false;
   }
 });
 
@@ -148,26 +147,14 @@ document.getElementById('downloadCode').addEventListener('click', function (e) {
   let date = today.toISOString().slice(0, 10);
   let time = ("0" + today.getHours()).slice(-2) + "-" + ("0" + today.getMinutes()).slice(-2)
 
-  let grid = document.getElementById("pageLayout").value.split('x')
-  let portrait = grid[0] == grid[1];
-  const doc = new jsPDF({
-    orientation: portrait ? "portrait" : "landscape",
-  });
-  doc.addImage(document.getElementById('eventqrcode').toDataURL("image/png"), 'PNG', 0, 0, doc.internal.pageSize.getWidth(), doc.internal.pageSize.getHeight(), "", 'FAST');
-  doc.save(`Event_QR_Codes_Date_${date}_Time_${time}`);
+  PDF.single.save(`Event_QR_Codes_Date_${date}_Time_${time}`);
 });
 
 document.getElementById('printCode').addEventListener('click', function (e) {
   e.preventDefault();
 
-  let grid = document.getElementById("pageLayout").value.split('x')
-  let portrait = grid[0] == grid[1];
-  const doc = new jsPDF({
-    orientation: portrait ? "portrait" : "landscape",
-  });
-  doc.addImage(document.getElementById('eventqrcode').toDataURL("image/png"), 'PNG', 0, 0, doc.internal.pageSize.getWidth(), doc.internal.pageSize.getHeight(), "", 'FAST');
-  doc.autoPrint();
-  let blob = doc.output("blob");
+  PDF.single.autoPrint();
+  let blob = PDF.single.output("blob");
   window.open(URL.createObjectURL(blob), '_blank');
 
 });
@@ -176,23 +163,11 @@ document.getElementById('downloadMultiCode').addEventListener('click', function 
   e.preventDefault();
   document.getElementById("modal").classList.remove('d-none'); 
   setTimeout(() => {
-    let pages = document.querySelectorAll('#qrContainer canvas')
     let today = new Date();
     let date = today.toISOString().slice(0, 10);
     let time = ("0" + today.getHours()).slice(-2) + "-" + ("0" + today.getMinutes()).slice(-2)
 
-    let grid = document.getElementById("pageTemplate").value.split('x')
-    let portrait = grid[0] == grid[1];
-    const doc = new jsPDF({
-      orientation: portrait ? "portrait" : "landscape",
-    });
-    let width = doc.internal.pageSize.getWidth();
-    let height = doc.internal.pageSize.getHeight();
-    pages.forEach((page, index) => {
-      doc.addImage(page.toDataURL("image/jepg"), 'JEPG', 0, 0, width, height, "", 'FAST');
-      if (index < pages.length - 1) doc.addPage();
-    })
-    doc.save(`Event_QR_Codes_Date_${date}_Time_${time}`);
+    PDF.multi.save(`Event_QR_Codes_Date_${date}_Time_${time}`);
     document.getElementById("modal").classList.add('d-none');
   }, 500)
 
@@ -201,20 +176,8 @@ document.getElementById('downloadMultiCode').addEventListener('click', function 
 document.getElementById('printMultiCode').addEventListener('click', function (e) {
   e.preventDefault();
 
-  let pages = document.querySelectorAll('#qrContainer canvas')
-  let grid = document.getElementById("pageTemplate").value.split('x')
-  let portrait = grid[0] == grid[1];
-  const doc = new jsPDF({
-    orientation: portrait ? "portrait" : "landscape",
-  });
-  let width = doc.internal.pageSize.getWidth();
-  let height = doc.internal.pageSize.getHeight();
-  pages.forEach((page, index) => {
-    doc.addImage(page.toDataURL("image/png"), 'PNG', 0, 0, width, height);
-    if (index < pages.length - 1) doc.addPage();
-  })
-  doc.autoPrint();
-  let blob = doc.output("blob");
+  PDF.multi.autoPrint();
+  let blob = PDF.multi.output("blob");
   window.open(URL.createObjectURL(blob), '_blank');
 });
 
@@ -480,7 +443,7 @@ async function GenerateQRCode(grid, description, address, defaultcheckinlengthMi
         canvas.style.maxWidth = "100%"
         ctx.drawImage(backgroundImage, 0, 0);
         ctx.drawImage(qr, 275, 230);
-        if(list) QR_LIST.push(canvas)
+        if(list) QR_LIST.push({QR: canvas, description: description, address: address})
         else resolve(canvas);
       });
     } else {
@@ -495,10 +458,6 @@ async function GenerateQRCode(grid, description, address, defaultcheckinlengthMi
 function PrintLayout(QR) {
   let title = ["CHECKEN SIE EIN.", "STOPPEN SIE DAS VIRUS."]
   let body = ["Nutzen Sie die Corona-Warn-App! Scannen Sie den QR-Code und tragen", "Sie aktiv dazu bei, mögliche Infektionsketten schnell und effektiv", "zu durchbrechen."]
-
-  let today = new Date();
-  let date = today.toISOString().slice(0, 10);
-  let time = ("0" + today.getHours()).slice(-2) + "-" + ("0" + today.getMinutes()).slice(-2)
 
   let description = document.getElementById('description').value;
   let address = document.getElementById('address').value;
@@ -516,82 +475,91 @@ function PrintLayout(QR) {
 
   let width = doc.internal.pageSize.getWidth();
   let height = doc.internal.pageSize.getHeight();
-  let headFontSize, titleFontSize,bodyFontSize, lineHeight;
+  let headFontSize, titleFontSize,bodyFontSize, lineHeightTitle, lineHeightBody;
 
   switch(document.getElementById("pageLayout").value) {
     case "1x1":
       headFontSize = 14;
-      titleFontSize = 24;
-      bodyFontSize = 14;
-      lineHeight = 1.5;
+      titleFontSize = 30;
+      bodyFontSize = 18.5;
+      lineHeightTitle = 1.3;
+      lineHeightBody = 1.2;
       break;
     case "2x1":
-      headFontSize = 14;
-      titleFontSize = 18;
-      bodyFontSize = 10;
-      lineHeight = 1.5;
+      headFontSize = 13;
+      titleFontSize = 21.5;
+      bodyFontSize = 13.1;
+      lineHeightTitle = 1.3;
+      lineHeightBody = 1.2;
       break;
     case "2x2":
       headFontSize = 10;
-      titleFontSize = 12;
-      bodyFontSize = 7;
-      lineHeight = 1.5;
+      titleFontSize = 15.3;
+      bodyFontSize = 9.3;
+      lineHeightTitle = 1.2;
+      lineHeightBody = 1.1;
       break;
     case "3x3":
-      headFontSize = 10;
-      titleFontSize = 8;
-      bodyFontSize = 5;
-      lineHeight = 1.3;
+      headFontSize = 6;
+      titleFontSize = 10;
+      bodyFontSize = 6.2;
+      lineHeightTitle = 1.3;
+      lineHeightBody = 1.2;
       break;
     case "4x2":
-      headFontSize = 10;
-      titleFontSize = 9;
-      bodyFontSize = 5;
-      lineHeight = 1.3;
+      headFontSize = 6;
+      titleFontSize = 10.7;
+      bodyFontSize = 6.5;
+      lineHeightTitle = 1.3;
+      lineHeightBody = 1.2;
       break;
     case "4x4":
-      headFontSize = 8;
-      titleFontSize = 6;
-      bodyFontSize = 4;
-      lineHeight = 1.35;
+      headFontSize = 5;
+      titleFontSize = 7.7;
+      bodyFontSize = 4.7;
+      lineHeightTitle = 1.2;
+      lineHeightBody = 1.2;
       break;
     case "5x5":
-      headFontSize = 6;
-      titleFontSize = 24;
-      bodyFontSize = 24;
-      lineHeight = 1.5;
+      headFontSize = 4;
+      titleFontSize = 6;
+      bodyFontSize = 3.7;
+      lineHeightTitle = 1.3;
+      lineHeightBody = 1.1;
       break;
     case "6x3":
-      headFontSize = 7;
-      titleFontSize = 24;
-      bodyFontSize = 24;
-      lineHeight = 1.5;
+      headFontSize = 5;
+      titleFontSize = 7.3;
+      bodyFontSize = 4.4;
+      lineHeightTitle = 1.2;
+      lineHeightBody = 1.2;
       break;
   }
-  doc.setLineHeightFactor(lineHeight);
+
   for (let r = 0; r < row; r++) {
     for (let c = 0; c < col; c++) {
       doc.addImage(QR, 'PNG', (width / col) * c, (height / row) * r, width / col, height / row, "", 'FAST');
 
-      doc.setTextColor('#000000');
       doc.setFontSize(headFontSize)
+      doc.setFont("helvetica");
+      doc.setTextColor('#000000');
       doc.setLineHeightFactor(1);
       doc.text([description, address], ((width/col) * c) + (width / col) * 0.1395, ((height/row) * r) + (height / row) * 0.6242)
-
-      doc.setLineHeightFactor(lineHeight);
 
       doc.setFontSize(titleFontSize);
       doc.setFont("futura-condensed-extrabold_bigfontsite.com");
       doc.setTextColor('#007099');
+      doc.setLineHeightFactor(lineHeightTitle);
       doc.text(title, ((width/col) * c) + (width / col) * 0.1295, ((height/row) * r) + (height / row) * 0.70);
 
       doc.setFontSize(bodyFontSize);
       doc.setFont("futura-condensedmedium_bigfontsite.com");
       doc.setTextColor('#404040');
+      doc.setLineHeightFactor(lineHeightBody);
       doc.text(body, ((width/col) * c) + (width / col) * 0.1295, ((height/row) * r) + (height / row) * 0.785);
     }
   }
-  doc.save(`Event_QR_Codes_Date_${date}_Time_${time}`);
+  PDF.single = doc;
 }
 
 async function GenerateMultiQRCode(data) {
@@ -613,16 +581,24 @@ async function GenerateMultiQRCode(data) {
 
 async function printQRsOnPage() {
   return new Promise((resolve) => {
+    let title = ["CHECKEN SIE EIN.", "STOPPEN SIE DAS VIRUS."]
+    let body = ["Nutzen Sie die Corona-Warn-App! Scannen Sie den QR-Code und tragen", "Sie aktiv dazu bei, mögliche Infektionsketten schnell und effektiv", "zu durchbrechen."]
+
     let grid = document.getElementById("pageTemplate").value.split('x')
     let col, row;
     col = grid[0];
     row = grid[1];
     let pageCapacity = row * col;
     let pagesNeeded = Math.ceil(QR_LIST.length / pageCapacity);
-    let pages = [];
 
-    let container = document.getElementsByClassName("slick-track")[0];
-    container.innerHTML = '';
+    let portrait = col == row;
+    const doc = new jsPDF({
+      orientation: portrait ? "portrait" : "landscape",
+    });
+
+    let width = doc.internal.pageSize.getWidth();
+    let height = doc.internal.pageSize.getHeight();
+    let headFontSize, titleFontSize,bodyFontSize, lineHeightTitle, lineHeightBody;
 
     //Reset generated info under carousel
     let info = document.getElementById("qrsGeneratedInfo");
@@ -638,115 +614,102 @@ async function printQRsOnPage() {
         break;
     }
 
-    //Start to print depend of the layout selected
+    switch(document.getElementById("pageTemplate").value) {
+      case "1x1":
+        headFontSize = 14;
+        titleFontSize = 30;
+        bodyFontSize = 18.5;
+        lineHeightTitle = 1.3;
+        lineHeightBody = 1.2;
+        break;
+      case "2x1":
+        headFontSize = 13;
+        titleFontSize = 21.5;
+        bodyFontSize = 13.1;
+        lineHeightTitle = 1.3;
+        lineHeightBody = 1.2;
+        break;
+      case "2x2":
+        headFontSize = 10;
+        titleFontSize = 15.3;
+        bodyFontSize = 9.3;
+        lineHeightTitle = 1.2;
+        lineHeightBody = 1.1;
+        break;
+      case "3x3":
+        headFontSize = 6;
+        titleFontSize = 10;
+        bodyFontSize = 6.2;
+        lineHeightTitle = 1.3;
+        lineHeightBody = 1.2;
+        break;
+      case "4x2":
+        headFontSize = 6;
+        titleFontSize = 10.7;
+        bodyFontSize = 6.5;
+        lineHeightTitle = 1.3;
+        lineHeightBody = 1.2;
+        break;
+      case "4x4":
+        headFontSize = 5;
+        titleFontSize = 7.7;
+        bodyFontSize = 4.7;
+        lineHeightTitle = 1.2;
+        lineHeightBody = 1.2;
+        break;
+      case "5x5":
+        headFontSize = 4;
+        titleFontSize = 6;
+        bodyFontSize = 3.7;
+        lineHeightTitle = 1.3;
+        lineHeightBody = 1.1;
+        break;
+      case "6x3":
+        headFontSize = 5;
+        titleFontSize = 7.3;
+        bodyFontSize = 4.4;
+        lineHeightTitle = 1.2;
+        lineHeightBody = 1.2;
+        break;
+    }
+    // //Start to print depend of the layout selected
     let i = 0;
     for (let pagem = 0; pagem < pagesNeeded; pagem++) {
       if (i < QR_LIST.length) {
-        let canvas = document.createElement("canvas");
-        canvas.className = "eventqr-preview";
-        let ctx = canvas.getContext('2d');
-        let resolution = Math.sqrt(col * row) / 2;
-        let width = 1654 * resolution;
-        let height = 2339 * resolution;
-        if (col == row) {
-          ctx.width = width;
-          ctx.height = height;
-          canvas.width = width;
-          canvas.height = height;
-          canvas.style.maxWidth = "100%";
-        } else {
-          ctx.width = height;
-          ctx.height = width;
-          canvas.width = height;
-          canvas.height = width;
-          canvas.style.maxWidth = "100%";
-        }
-
         for (let r = 0; r < row; r++) {
           for (let c = 0; c < col; c++) {
-            if (i < QR_LIST.length) {
-              ctx.drawImage(QR_LIST[i], (canvas.width / col) * c, (canvas.height / row) * r, canvas.width / col, canvas.height / row);
+              doc.addImage(QR_LIST[i].QR, 'PNG', (width / col) * c, (height / row) * r, width / col, height / row, "", 'FAST');
+
+              doc.setFontSize(headFontSize)
+              doc.setFont("helvetica");
+              doc.setTextColor('#000000');
+              doc.setLineHeightFactor(1);
+              doc.text([QR_LIST[i].description, QR_LIST[i].address], ((width/col) * c) + (width / col) * 0.1395, ((height/row) * r) + (height / row) * 0.6242)
+
+              doc.setFontSize(titleFontSize);
+              doc.setFont("futura-condensed-extrabold_bigfontsite.com");
+              doc.setTextColor('#007099');
+              doc.setLineHeightFactor(lineHeightTitle);
+              doc.text(title, ((width/col) * c) + (width / col) * 0.1295, ((height/row) * r) + (height / row) * 0.70);
+
+              doc.setFontSize(bodyFontSize);
+              doc.setFont("futura-condensedmedium_bigfontsite.com");
+              doc.setTextColor('#404040');
+              doc.setLineHeightFactor(lineHeightBody);
+              doc.text(body, ((width/col) * c) + (width / col) * 0.1295, ((height/row) * r) + (height / row) * 0.785);
               i++;
-            }
           }
         }
-
-        if (col != row) {
-          ctx.translate(-canvas.width / 2, -canvas.height / 2);
-          ctx.rotate(-Math.PI / 2);
-        }
-        pages.push(canvas)
       }
+      if(pagem < pagesNeeded-1) doc.addPage();
     }
+    PDF.multi = doc;
     document.getElementById('printMultiCode').disabled = false;
     // Active download button
     document.getElementById('downloadMultiCode').disabled = false;
     document.getElementById('multieventplaceholder').classList.add('d-none');
 
-    let data = { "pages": pages, "container": container, "totalQR": QR_LIST.length }
+    let data = { "pages": pagesNeeded, "totalQR": QR_LIST.length }
     return resolve(data);
   });
-}
-
-async function printPages(data) {
-  data.pages.forEach((printed) => {
-    $('.qr-slider').slick('slickAdd', printed);
-  })
-
-  let info = document.getElementById("qrsGeneratedInfo");
-  let text = info.textContent;
-  text = text.replace('#', data.pages.length);
-  text = text.replace('$', data.totalQR);
-  info.textContent = text;
-  let container = document.getElementById("qrContainer")
-  container.classList.remove('d-none');
-  info.classList.remove('d-none');
-
-  let infoContainer = document.getElementById("qrsGeneratedInfoContainer");
-  if (data.pages.length == 1) {
-    infoContainer.classList.remove('mt-4');
-    container.classList.remove('slick-dotted');
-  }
-  else {
-    infoContainer.classList.add('mt-4');
-    container.classList.add('slick-dotted');
-
-    let slider = $(".qr-slider");
-
-    function loadSliderDotClasses(stickSlider) {
-      let dot = stickSlider[0].querySelector(".slick-dots li.slick-active"),
-        dotsLength = stickSlider[0].querySelectorAll(".slick-dots li").length,
-        dotSize1 = "dot-size-1",
-        dotSize2 = "dot-size-2",
-        dotSize3 = "dot-size-3";
-        let activeIndex = Array.from(dot.parentNode.children).indexOf(dot);
-        stickSlider[0].querySelectorAll(".slick-dots li").forEach((li, index) => {
-          li.classList.remove(dotSize1, dotSize2, dotSize3, "show");
-        });
-        stickSlider[0].querySelectorAll(".slick-dots li").forEach((li, index) => {
-          let difference = activeIndex - index;
-          if (difference === -2 && dotsLength + difference > -1) {
-              li.classList.add(dotSize2, "show");
-          }
-          if (difference === -1 && dotsLength + difference > -1) {
-              li.classList.add(dotSize3, "show");
-          }
-          if (difference === 1 && dotsLength - difference < dotsLength) {
-              li.classList.add(dotSize3, "show");
-          }
-          if (difference === 2 && dotsLength - difference < dotsLength) {
-              li.classList.add(dotSize2, "show");
-          }
-        });
-      dot.classList.add("show");
-    }
-
-    loadSliderDotClasses(slider);
-
-    slider.find(".slick-arrow").on("click", function (e) {
-      loadSliderDotClasses(slider);
-    });
-  }
-  document.getElementById("modal").classList.add('d-none');
-  document.getElementById('generateMultiQR').disabled = false;
 }
