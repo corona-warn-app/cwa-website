556e57ed01fcf5f2455c151d654468434856e8fa
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2022-06-01 22:30:02 +0200

    Remove viewport from plugins\index.js

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,24 +1,6 @@
+/// <reference types="cypress" />
+// eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
-
-  const viewport = { viewportWidth: 1000, viewportHeight: 600 }
-  // prevent different screen sizes
-  on('before:browser:launch', (browser = {}, launchOptions) => {
-    switch (browser.name) {
-      //browser.family === 'chromium' && browser.name !== 'electron')
-      case 'chrome':
-        launchOptions.args.push(`--window-size=$ viewport.viewportWidth},$ viewport.viewportHeight}`)
-        /*
-        launchOptions.push('--cast-initial-screen-width=1600')
-        launchOptions.push('--cast-initial-screen-height=900')
-          */
-        break
-      case 'electron':
-        launchOptions.preferences.width = viewport.viewportWidth
-        launchOptions.preferences.height = viewport.viewportHeight
-        break
-    }
-    return launchOptions
-  })
-
-  return config;
-};
+  // `on` is used to hook into various events Cypress emits
+  // `config` is the resolved Cypress config
+}
