d2a561c76bac4bd30a2f5c8f4430983f9bcb6541
Brian Fernández Muñiz <brifemu@gmail.com>
2022-06-02 08:10:18 +0200

    fix test - got stucked with heavy videos

--- cypress/integration/check_videos.js
+++ cypress/integration/check_videos.js
@@ -40,26 +40,20 @@ context("Check for broken videos", () => {
         cy.writeFile("cypress/logs/broken_videos_result.txt", "==================== Broken videos ====================\n")
     })
 
-    pages.forEach(page => {
+    pages.map(page => {
+        console.log(page);
         it(`"${page}" - Check for broken videos`, () => {
+            console.log("enter in test")
             cy.visit({log: false, url: page});
-            cy.get('#main').then($main => {
+            cy.get('main').then($main => {
                 if ($main.find('video').length > 0) {
-                    cy.get('video').each(video => {
-                        cy.request({
-                            failOnStatusCode: false, 
-                            log: false,
-                            url: video.prop('src')
-                        }).then((response) => {
-                            softExpect(response.status === 200 ? true : false, "Link: " + video.prop('src')).to.eq(true)
-                            if(response.status !== 200) {
-                                cy.readFile("cypress/logs/broken_videos_result.txt")
-                                .then((text) => {
-                                    cy.writeFile("cypress/logs/broken_videos_result.txt", `${text}\n[RESPONSE ${response.status}] ${video.prop('src')} on '${page}' `, {flags: 'as+'})
-                                })
-                            }
-                        })
+                    cy.get('video')
+                    .should(($video) => {
+                        expect(Number.isNaN($video.prop('duration')), `duration should be a number - broken '${$video.prop('src')}' on '${page}' `).to.eq(false);
+                        expect($video.prop('duration')).to.be.gt(0)
                     })
+                    .and('have.prop', 'paused', true)
+                    .and('have.prop', 'ended', false)
                 }
             })
         })
@@ -69,32 +63,23 @@ context("Check for broken videos", () => {
 context("Check for broken links on entries", () => {
     const pages = ['/de/blog/','/en/blog/','/de/science/', '/en/science/']
 
-    pages.forEach(sub => {
-        it(`"${sub}" entries - Check for broken videos`, () => {
-            cy.visit({log: false, url: sub} )
+    pages.map(page => {
+        it(`"${page}" entries - Check for broken videos`, () => {
+            cy.visit({log: false, url: page} )
             cy.get("a.blog-read-more").each(url => {
                 cy.visit({log: false, url: url.prop('href')} )
                 cy.get('main').then($main => {
-                    if ($main.find('source').length > 0) {
-                        cy.get('source').each(video => {
-                            cy.request({
-                                failOnStatusCode: false, 
-                                log: false,
-                                url: video.prop('src'),
-                                timeout: 100000
-                        }).then((response) => {
-                            softExpect(response.status === 200 ? true : false, "Link: " + video.prop('src')).to.eq(true)
-                            if(response.status !== 200) {
-                                cy.readFile("cypress/logs/broken_videos_result.txt")
-                                .then((text) => {
-                                    cy.writeFile("cypress/logs/broken_videos_result.txt", `${text}\n[RESPONSE ${response.status}] ${video.prop('src')} on '${url.prop('href')}' `, {flags: 'as+'})
-                                })
-                            }
+                    if ($main.find('video').length > 0) {
+                        cy.get('video')
+                        .should(($video) => {
+                            expect(Number.isNaN($video.prop('duration')), `duration should be a number - broken '${$video.prop('src')}' on '${page}' `).to.eq(false);
+                            expect($video.prop('duration')).to.be.gt(0)
                         })
-                    })
-                } 
+                        .and('have.prop', 'paused', true)
+                        .and('have.prop', 'ended', false)
+                    }
+                })
             })
-        })  
         })
     })
 })
