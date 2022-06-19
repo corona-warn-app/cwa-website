dbda8ddb7743319655130d57c1743443b2222c1a
chrisenitan <enitanchris@gmail.com>
2022-06-19 13:43:10 +0200

    assert viewarea and cleanups

--- cypress/integration/faq.js
+++ cypress/integration/faq.js
@@ -2,56 +2,56 @@
 import * as selectors from "/cypress/fixtures/selectors/sel.json"
 import stringDe from "/cypress/fixtures/copy/stringDe.json"
 import stringEn from "/cypress/fixtures/copy/stringEn.json"
+let tests = [
+  {
+    views: ["iphone-6", "macbook-13"],
+    domain: "en",
+    string: stringEn,
+  },
+  {
+    views: ["iphone-6", "macbook-13"],
+    domain: "de",
+    string: stringDe,
+  },
+]
 
-describe("... Corona-Warn-App", () => {
-  //no idea what to call this
-  let tests = [
-    {
-      views: ["iphone-6", "macbook-13"],
-      domain: "en",
-      string: stringEn,
-    },
-    {
-      views: ["iphone-6", "macbook-13"],
-      domain: "de",
-      string: stringDe,
-    },
-  ]
-
+describe("Test CWA FAQ Pages", () => {
   tests.forEach(({ views, domain, string }) => {
     views.forEach((view) => {
-      describe(`Tests FAQ page features on ${domain}`, () => {
+      describe(`Page features on ${domain} with ${view}`, () => {
         beforeEach("Prepare device and language versions", () => {
           cy.viewport(view)
           cy.visit("/" + domain + "/faq/")
         })
 
-        it.skip(`Loads faq page with search features on ${domain} for ${view}`, () => {
+        Cypress.Commands.add("searchFaq", (search) => {
+          cy.get(selectors.faq.textField).type(search.text)
+          cy.get(selectors.faq.topicField).select(search.topic)
+          cy.get(selectors.faq.submitButton).click()
+        })
+
+        it(`Loads faq page with search features on ${domain} for ${view}`, () => {
           cy.get(selectors.faq.headline).contains(string.faq.top)
           cy.get(selectors.faq.textField)
           cy.get(selectors.faq.topicField)
           cy.get(selectors.faq.submitButton).should("not.be.disabled")
         })
 
-        it.skip(`Can interact with faq page search features on ${domain} for ${view}`, () => {
-          cy.get(selectors.faq.textField).type("Test")
-          cy.get(selectors.faq.topicField).select(string.faq.testTopic)
+        it(`Can interact with faq page search features on ${domain} for ${view}`, () => {
           cy.get(selectors.faq.submitButton).should("not.be.disabled")
+          cy.searchFaq(string.faq.testSearch)
         })
 
-        it.skip(`Can display relevant search results on ${domain} for ${view}`, () => {
-          cy.get(selectors.faq.textField).type("Test")
-          cy.get(selectors.faq.topicField).select(string.faq.testTopic)
-          cy.get(selectors.faq.submitButton).click()
-          cy.get(selectors.faq.resultHeadline).contains(string.faq.testTopic)
+        it(`Can display relevant search results on ${domain} for ${view}`, () => {
+          cy.searchFaq(string.faq.testSearch)
+          cy.get(selectors.faq.resultHeadline).contains(string.faq.testSearch.topic)
           cy.url().should("include", `${domain}/faq/results/?search=Test`)
         })
 
         it(`Can display specific faq article on ${domain} for ${view}`, () => {
           cy.visit(`${domain}/faq/results/#further_details`)
           cy.url().should("include", `${domain}/faq/results/#further_details`)
-          //wip in view assertion
-          cy.get(selectors.faq.furtherDetailsDiv).scrollIntoView().should("be.visible")
+          cy.get(selectors.faq.furtherDetailsDiv).find(".accordion-faq-header").should("have.class", "active")
           cy.get(selectors.faq.furtherDetailsDiv).contains(`${string.faq.resultAssertion}`)
         })
       })
