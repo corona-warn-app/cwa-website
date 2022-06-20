bc63518c0556526fc55f798c8820e579bb05eea2
Brian Fernández Muñiz <brifemu@gmail.com>
2022-06-20 13:02:39 +0200

    Include checking of external links from Android and iOS repos

--- cypress/integration/app_to_web.js
+++ cypress/integration/app_to_web.js
@@ -1,11 +1,54 @@
 /// <reference types="Cypress" />
+const { softAssert, softExpect } = chai;
 
 describe("Test cwa-webserver links used by Corona-Warn-App", () => {
 
     let languages = ["en", "de"];
     let links;
+
+    // removeMatches => Remove n first matches from result, e.g. for removing XML validation URLs
+    const fetchs = [
+        {
+            title: "[EN] Android external links",
+            url: "https://raw.githubusercontent.com/corona-warn-app/cwa-app-android/main/Corona-Warn-App/src/main/res/values/links.xml",
+            removeMatches: 2,
+            result: []
+        },
+        {
+            title: "[DE] Android external links",
+            url: "https://raw.githubusercontent.com/corona-warn-app/cwa-app-android/main/Corona-Warn-App/src/main/res/values-de/links.xml",
+            removeMatches: 2,
+            result: []
+        },
+        {
+            title: "[EN] iOS external links",
+            url: "https://raw.githubusercontent.com/corona-warn-app/cwa-app-ios/main/src/xcode/ENA/ENA/Resources/Localization/en.lproj/Localizable.links.strings",
+            removeMatches: 0,
+            result: []
+        },
+        {
+            title: "[DE] iOS external links",
+            url: "https://raw.githubusercontent.com/corona-warn-app/cwa-app-ios/main/src/xcode/ENA/ENA/Resources/Localization/de.lproj/Localizable.links.strings",
+            removeMatches: 0,
+            result: []
+        }
+    ]
+
+    const hashRegex = /[#][a-zA-Z0-9-_]{1,256}/g;
     before(() => {
         cy.fixture('appToWebLinks').then(webLinks => links = webLinks);
+
+        fetchs.forEach(query => {
+            fetch(query.url)
+            .then(response => response.text())
+            .then(data => {
+                const regex = /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi;
+                query.result = data.match(regex);
+                if(query.removeMatches > 0) {
+                    query.result = query.result.splice(query.removeMatches);
+                }
+            });
+        })
     });
 
     it("Test FAQ direct links", () => {
@@ -55,4 +98,44 @@ describe("Test cwa-webserver links used by Corona-Warn-App", () => {
             });
         });
     });
+
+    describe("Check external links", () => {
+        fetchs.forEach(query => {
+            it(query.title, () => {
+                if(query.result) {
+                    query.result.forEach(url => {
+                        const hash = url.match(hashRegex);
+                        // Check if the URL has a hash, if it has one, we need to check if an element with the hash as ID exists
+                        if(hash) {
+                            // Check if is a CWA URL, if not, we can't check if the element exists
+                            if(url.includes('coronawarn.app')) {
+                                url = url.replace('www.coronawarn.app', '')
+                                // Check if is a redirect, if it is, we avoid checking it cause has been checked before
+                                if(!links.faqRedirect.includes(hash.toString())) {
+                                    // Check if is an URL heading to the old FAQ page, if yes, redirect to the new one cause CWA site do it on visit
+                                    if(url.includes('/faq/')) {
+                                        url = url.replace('/faq/', '/faq/results/')
+                                    }
+                                    cy.visit({log: false, url: url} );
+                                    cy.get('body').then($body => {
+                                        const elementInBody = $body.find(`${hash}`).length > 0 ? true : false;
+                                        softExpect(elementInBody, "Link: " + url + " looking at: "+hash).to.eq(true)
+                                    })
+                                }
+                            }
+                        } else {
+                            cy.request({
+                                failOnStatusCode: false, 
+                                log: false,
+                                url: url
+                            }).then((response) => {
+                                softExpect(response.status == 200 || response.status == 429 ? true : false, "Link: " + url).to.eq(true)
+                            })
+                        }
+                    });
+                }
+            })
+        })    
+    })
+    
 });
