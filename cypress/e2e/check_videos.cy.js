d60b79ce94354a92a1089def3d8d4c470396c7e0
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2023-02-16 11:49:36 +0100

    Correct text on video check of entries

--- cypress/e2e/check_videos.cy.js
+++ cypress/e2e/check_videos.cy.js
@@ -36,7 +36,7 @@ context("Check for broken videos", () => {
     })
 })
 
-context("Check for broken links on blog/science entries", () => {
+context("Check for broken videos on blog/science entries", () => {
     const pages = ['/de/blog/','/en/blog/','/de/science/', '/en/science/']
 
     pages.map(page => {
