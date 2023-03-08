0194ad195b6017e28ecfbc5ea58df1b9a1553b55
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2023-03-08 14:15:32 +0100

    Exclude buergertest.ecocare.center from link test (#3418)

--- cypress/e2e/hybrid/check_links.cy.js
+++ cypress/e2e/hybrid/check_links.cy.js
@@ -2,6 +2,7 @@ const { softAssert, softExpect } = chai;
 
 const noCheckHosts = [
   'developer.apple.com',       // HTTP 403 forbidden issue on GitHub
+  'buergertest.ecocare.center', // HTTP 404 not found - testing site removed
   'ec.europa.eu',              // HTTP 502 bad gateway
   'health.ec.europa.eu',       // HTTP 502 bad gateway
   'onlinelibrary.wiley.com',   // HTTP 503 service unavailable
