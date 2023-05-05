5a7c185f86ce5e1368d543164216ca28357de205
thorbenkuro <71256882+thorbenkuro@users.noreply.github.com>
2023-05-05 10:23:03 +0200

    Fix failing cypress test regarding faq-links and enable logging (#3490)

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,6 +1,13 @@
 /// <reference types="cypress" />
 // eslint-disable-next-line no-unused-vars
 module.exports = (on, config) => {
+  // This enables us to log to console by using cy.task("log", "Message to log")
+  on('task', {
+    log(message) {
+      console.log(message);
+      return null;
+    },
+  });
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
